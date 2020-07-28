0822882c0c07d7a2532ba0e4d99fe149642f5410
TristanDCP <tristandacostapinto@gmail.com>
2020-07-28 11:04:39 +0200

    setup boucle

--- index.php
+++ index.php
@@ -2,6 +2,10 @@
 require_once('Character.php');
 require_once('Hero.php');
 require_once('Orc.php');
+$hero = new Hero(2000,0, 'Shear Trigger', 250, 'Crystal Mail', 600);
+$ennemi = new Orc(500,0);
+$orcAttackValue = $ennemi->randomOrcAttackDamage();
+$increaseValue = +30;
 
 ?>
 
@@ -14,15 +18,14 @@ require_once('Orc.php');
 </head>
 <body>
 <?php
-$hero = new Hero(2000,0, 'Shear Trigger', 250, 'Crystal Mail', 600);
-$ennemi = new Orc(500,0);
-$orcAttackValue = $ennemi->randomOrcAttackDamage();
-echo 'Tu as maintenant : ' .$hero->getHealth(). ' point de vie <br>';
-echo 'Monsieur Orc possède : ' .$ennemi->getDamage(). ' points attaque';
+
+echo 'Le héros possède  ' .$hero->getHealth(). ' point de vie de base <br>';
+echo 'L\'Orc possède  ' .$ennemi->getDamage(). ' points d\'attaque';
 
 while($hero->attacked($ennemi->getDamage())){
     $hero->setHealth($newHealth);
-    
+    $hero->rageIncrease($increaseValue);
+    echo 'Tu as maintenant : ' .$hero->getHealth(). ' point de vie <br>';
 }
 ?>
 
